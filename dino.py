fc85345cea8fae8b009f25db5fa22b6bde895140
Matthias Duriez <matthiasduriez@orange.fr>
2021-04-24 18:44:11 +0200

    adding replay + improving obstacles

--- dino.py
+++ dino.py
@@ -7,7 +7,7 @@ pygame.font.init()
 
 
 
-DINO_IMG = [pygame.image.load(os.path.join("images/dino",'dino_3.png')),pygame.image.load(os.path.join("images/dino",'dino_4.png')),pygame.image.load(os.path.join("images/dino",'dino_7.png')),pygame.image.load(os.path.join("images/dino",'dino_8.png'))]
+DINO_IMG = [pygame.image.load(os.path.join("images/dino",'dino_3.png')),pygame.image.load(os.path.join("images/dino",'dino_4.png')),pygame.image.load(os.path.join("images/dino",'dino_7.png')),pygame.image.load(os.path.join("images/dino",'dino_8.png')),pygame.image.load(os.path.join("images/dino",'dino_6.png'))]
 CACTUS_IMG = [pygame.image.load(os.path.join("images/cacti","cacti_large_1.png"))]
 PTERA_IMG = [pygame.image.load(os.path.join("images/ptera","ptera_1.png"))]
 GROUND_IMG = pygame.image.load(os.path.join("images","ground.png"))
@@ -34,6 +34,7 @@ class Dino:
         self.height = self.y
         self.squating= False
         self.jumping = False
+        self.died = False
     
     def jump(self):
         self.vel = self.VEL
@@ -43,22 +44,20 @@ class Dino:
     
     def squat(self):
         self.vel = self.VEL
-        self.squating = True
 
 
     def input_handler(self, userInput):
         if userInput[pygame.K_UP] and not self.jumping:
             self.jumping = True
             self.squating = False
-        if userInput[pygame.K_DOWN] and not self.jumping:
+        if userInput[pygame.K_DOWN]:
             self.squating = True
-            self.jumping = False
         if not(userInput[pygame.K_DOWN] and self.squating):
             #self.jumping = False
             self.squating = False
         if self.jumping and not self.jumped:
             self.jump()
-        if self.squating:
+        if self.squating and not self.jumped:
             self.squat()
         
 
@@ -85,28 +84,33 @@ class Dino:
         return pygame.mask.from_surface(self.img)
 
     def draw(self,win):
-        self.img_count +=1
-        self.img_index = 0
-        if (self.squating):
-            self.img_index = 2
-        if not(self.jumping):
-            self.y = WIN_HEIGHT-70-DINO_IMG[self.img_index].get_height()
-        if self.img_count < self.RUN_SPEED:
-            self.img = DINO_IMG[self.img_index]
-        elif self.img_count < self.RUN_SPEED*2:
-            self.img = DINO_IMG[self.img_index+1]
-        elif self.img_count < self.RUN_SPEED*2+1:
-            self.img = DINO_IMG[self.img_index]
-            self.img_count=0
+        if not self.died:
+            self.img_count +=1
+            if (self.jumping or not self.squating):
+                self.img_index = 0
+            if (self.squating and not self.jumping):
+                self.img_index = 2
+            if not(self.jumping):
+                self.y = WIN_HEIGHT-70-DINO_IMG[self.img_index].get_height()
+            if self.img_count < self.RUN_SPEED:
+                self.img = DINO_IMG[self.img_index]
+            elif self.img_count < self.RUN_SPEED*2:
+                self.img = DINO_IMG[self.img_index+1]
+            elif self.img_count < self.RUN_SPEED*2+1:
+                self.img = DINO_IMG[self.img_index]
+                self.img_count=0
+        if self.died:
+            self.img = DINO_IMG[4]
         win.blit(self.img, (self.x,self.y))
 
 class Obstacle:
+    VEL = 7 
     IMG = CACTUS_IMG+PTERA_IMG
     def __init__(self,x,typeObstacle):
         self.x = x
         self.type = typeObstacle
         self.img = self.IMG[typeObstacle]
-        self.vel = 7
+        self.vel = self.VEL
         self.passed = False;
         if self.type == 0:
             self.y = WIN_HEIGHT-90-self.img.get_height()
@@ -114,12 +118,12 @@ class Obstacle:
             self.y = WIN_HEIGHT-100-self.img.get_height()-50
 
     def move(self,game_speed):
-        self.vel = game_speed
+        self.vel = self.VEL * game_speed
         self.x -= self.vel
     def collide(self,dino):
         dino_mask = dino.get_mask()
         obstacle_mask = pygame.mask.from_surface(self.img)
-        offset = (self.x - dino.x, self.y - round(dino.y))
+        offset = (round(self.x) - dino.x, self.y - round(dino.y))
         collide_point = dino_mask.overlap(obstacle_mask,offset)
         if collide_point:
             return True
@@ -127,21 +131,22 @@ class Obstacle:
 
         
     def draw(self,win):
+        print(self.x)
         win.blit(self.img, (self.x, self.y))
 
 class Ground:
     
     WIDTH = GROUND_IMG.get_width()
     IMG = GROUND_IMG
-
+    VEL = 7
     def __init__(self,y):
-        self.vel = 7
+        self.vel = self.VEL
         self.y = y
         self.x1 = 0
         self.x2 = self.WIDTH
 
     def move(self,game_speed):
-        self.vel = game_speed
+        self.vel = self.VEL * game_speed
         self.x1 -= self.vel
         self.x2 -= self.vel
 
@@ -160,14 +165,14 @@ def obstacle_gen(obstacles):
     if len(obstacles) == 2:
         if(obstacles[1].x>WIN_WIDTH-400):
             return obstacles
-        if(random.randrange(0,100)<=10):
-           obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2))) 
+        if(random.randrange(0,100)<=30):
+           obstacles.append(Obstacle(random.randrange(WIN_WIDTH+300,WIN_WIDTH+600),random.randrange(0,2))) 
     if len(obstacles) == 1:
-        if(obstacles[0].x>WIN_WIDTH-700):
+        if(obstacles[0].x>WIN_WIDTH-400):
             return obstacles
-        obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2))) 
+        obstacles.append(Obstacle(random.randrange(WIN_WIDTH+200,WIN_WIDTH+400),random.randrange(0,2))) 
     if len(obstacles) == 0:
-        obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2)))
+        obstacles.append(Obstacle(random.randrange(WIN_WIDTH+100,WIN_WIDTH+300),random.randrange(0,2))) 
     return obstacles
 def obstacle_delete(obstacles):
     rem = []
@@ -180,7 +185,7 @@ def obstacle_delete(obstacles):
             obstacles.remove(obstacle)
     return obstacles
 
-def draw_window(win,dino,ground,obstacles,score,game_speed):
+def draw_window(win,dino,ground,obstacles,score,game_speed,dino_died):
     win.fill([255,255,255])
     if len(obstacles)>0:
         for obstacle in obstacles:
@@ -191,6 +196,9 @@ def draw_window(win,dino,ground,obstacles,score,game_speed):
     win.blit(text,(10,50))
     ground.draw(win)
     dino.draw(win)
+    if dino_died:
+        text = STAT_FONT.render("GAME OVER. PRESS ENTER TO REPLAY",3, (0,0,0))
+        win.blit(text,((WIN_HEIGHT/2)-(text.get_height()/2),WIN_WIDTH/2-text.get_width()/2))
     pygame.display.update()
 
 def main():
@@ -199,12 +207,11 @@ def main():
     win.fill([255,255,255])
     clock = pygame.time.Clock()
     ground = Ground(WIN_HEIGHT-100)
-    died = False
     run = True
     dino = Dino(20)
     obstacles = [Obstacle(WIN_WIDTH+10,0)]
     score = 0
-    game_speed = 7
+    game_speed = 1
     
     while run:
         clock.tick(60)
@@ -214,15 +221,15 @@ def main():
                 run = False
                 pygame.quit()
                 quit()
-            dino.input_handler(pygame.key.get_pressed())
-        if died == False:
+        dino.input_handler(pygame.key.get_pressed())
+        if dino.died == False:
             score+=1
-            if (score%200==0):
-                game_speed+=1
+            if (score%20==0):
+                game_speed+=0.01
             if len(obstacles)>0:
                 for obstacle in obstacles:
                     if obstacle.collide(dino):
-                        died=True
+                        dino.died=True
                     obstacle.move(game_speed)
             obstacles = obstacle_delete(obstacles)
             if clock_tick_count==60:
@@ -230,5 +237,10 @@ def main():
                 clock_tick_count=0
             dino.move()
             ground.move(game_speed)
-        draw_window(win,dino,ground,obstacles,score,game_speed)
-main()
+        else:
+            if pygame.key.get_pressed()[pygame.K_RETURN]:
+                return True
+        draw_window(win,dino,ground,obstacles,score,game_speed,dino.died)
+replay = True
+while (replay):
+    replay = main()
