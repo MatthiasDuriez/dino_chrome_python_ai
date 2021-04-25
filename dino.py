aa3a0af3a1b708d0cff70d38bc7cf635da4a8569
Matthias Duriez <matthiasduriez@orange.fr>
2021-04-25 16:43:55 +0200

    adding neat ai

--- dino.py
+++ dino.py
@@ -16,6 +16,8 @@ BG_IMAGE = pygame.image.load(os.path.join("images","cloud.png"))
 WIN_HEIGHT = 700
 WIN_WIDTH = 1500
 
+GEN=0
+
 STAT_FONT = pygame.font.SysFont("comicsans",50)
 
 class Dino:
@@ -34,6 +36,7 @@ class Dino:
         self.height = self.y
         self.squating= False
         self.jumping = False
+        self.died = False
     
     def jump(self):
         self.vel = self.VEL
@@ -43,22 +46,20 @@ class Dino:
     
     def squat(self):
         self.vel = self.VEL
-        self.squating = True
 
 
-    def input_handler(self, userInput):
-        if userInput[pygame.K_UP] and not self.jumping:
+    def input_handler(self, jump , squat):
+        if jump>0.5 and not self.jumping:
             self.jumping = True
             self.squating = False
-        if userInput[pygame.K_DOWN] and not self.jumping:
+        if squat>0.5:
             self.squating = True
-            self.jumping = False
-        if not(userInput[pygame.K_DOWN] and self.squating):
+        if not(squat>0.5 and self.squating):
             #self.jumping = False
             self.squating = False
         if self.jumping and not self.jumped:
             self.jump()
-        if self.squating:
+        if self.squating and not self.jumped:
             self.squat()
         
 
@@ -66,11 +67,11 @@ class Dino:
         if self.jumped==True:
             self.y -= self.vel*2
             self.vel -= 0.6
-            if self.y >= self.base:
-                self.jumped=False
-                self.vel = self.VEL
-                self.y = self.base
-                self.jumping = False
+        if self.y >= self.base:
+            self.jumped=False
+            self.vel = self.VEL
+            self.y = self.base
+            self.jumping = False
             # self.tick_count += 1
             # d = self.vel*self.tick_count + 1.5*self.tick_count**1.9
             # if d >= 50:
@@ -85,28 +86,33 @@ class Dino:
         return pygame.mask.from_surface(self.img)
 
     def draw(self,win):
-        self.img_count +=1
-        self.img_index = 0
-        if (self.squating):
-            self.img_index = 2
-        if not(self.jumping):
-            self.y = WIN_HEIGHT-70-DINO_IMG[self.img_index].get_height()
-        if self.img_count < self.RUN_SPEED:
-            self.img = DINO_IMG[self.img_index]
-        elif self.img_count < self.RUN_SPEED*2:
-            self.img = DINO_IMG[self.img_index+1]
-        elif self.img_count < self.RUN_SPEED*2+1:
-            self.img = DINO_IMG[self.img_index]
-            self.img_count=0
+        if not self.died:
+            self.img_count +=1
+            if (self.jumping or not self.squating):
+                self.img_index = 0
+            if (self.squating and not self.jumping):
+                self.img_index = 2
+            if not(self.jumping):
+                self.y = WIN_HEIGHT-70-DINO_IMG[self.img_index].get_height()
+            if self.img_count < self.RUN_SPEED:
+                self.img = DINO_IMG[self.img_index]
+            elif self.img_count < self.RUN_SPEED*2:
+                self.img = DINO_IMG[self.img_index+1]
+            elif self.img_count < self.RUN_SPEED*2+1:
+                self.img = DINO_IMG[self.img_index]
+                self.img_count=0
+        if self.died:
+            self.img = DINO_IMG[4]
         win.blit(self.img, (self.x,self.y))
 
 class Obstacle:
+    VEL = 7 
     IMG = CACTUS_IMG+PTERA_IMG
     def __init__(self,x,typeObstacle):
         self.x = x
         self.type = typeObstacle
         self.img = self.IMG[typeObstacle]
-        self.vel = 7
+        self.vel = self.VEL
         self.passed = False;
         if self.type == 0:
             self.y = WIN_HEIGHT-90-self.img.get_height()
@@ -114,12 +120,12 @@ class Obstacle:
             self.y = WIN_HEIGHT-100-self.img.get_height()-50
 
     def move(self,game_speed):
-        self.vel = game_speed
+        self.vel = self.VEL * game_speed
         self.x -= self.vel
     def collide(self,dino):
         dino_mask = dino.get_mask()
         obstacle_mask = pygame.mask.from_surface(self.img)
-        offset = (self.x - dino.x, self.y - round(dino.y))
+        offset = (round(self.x) - dino.x, self.y - round(dino.y))
         collide_point = dino_mask.overlap(obstacle_mask,offset)
         if collide_point:
             return True
@@ -133,15 +139,15 @@ class Ground:
     
     WIDTH = GROUND_IMG.get_width()
     IMG = GROUND_IMG
-
+    VEL = 7
     def __init__(self,y):
-        self.vel = 7
+        self.vel = self.VEL
         self.y = y
         self.x1 = 0
         self.x2 = self.WIDTH
 
     def move(self,game_speed):
-        self.vel = game_speed
+        self.vel = self.VEL * game_speed
         self.x1 -= self.vel
         self.x2 -= self.vel
 
@@ -160,14 +166,14 @@ def obstacle_gen(obstacles):
     if len(obstacles) == 2:
         if(obstacles[1].x>WIN_WIDTH-400):
             return obstacles
-        if(random.randrange(0,100)<=10):
-           obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2))) 
+        if(random.randrange(0,100)<=30):
+           obstacles.append(Obstacle(random.randrange(WIN_WIDTH+300,WIN_WIDTH+600),random.randrange(0,2))) 
     if len(obstacles) == 1:
-        if(obstacles[0].x>WIN_WIDTH-700):
+        if(obstacles[0].x>WIN_WIDTH-400):
             return obstacles
-        obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2))) 
+        obstacles.append(Obstacle(random.randrange(WIN_WIDTH+200,WIN_WIDTH+400),random.randrange(0,2))) 
     if len(obstacles) == 0:
-        obstacles.append(Obstacle(WIN_WIDTH+10,random.randrange(0,2)))
+        obstacles.append(Obstacle(random.randrange(WIN_WIDTH+100,WIN_WIDTH+300),random.randrange(0,2))) 
     return obstacles
 def obstacle_delete(obstacles):
     rem = []
@@ -180,20 +186,34 @@ def obstacle_delete(obstacles):
             obstacles.remove(obstacle)
     return obstacles
 
-def draw_window(win,dino,ground,obstacles,score,game_speed):
+def draw_window(win,dinos,ground,obstacles,score,game_speed,gen,alive_nb):
     win.fill([255,255,255])
     if len(obstacles)>0:
         for obstacle in obstacles:
             obstacle.draw(win)
     text = STAT_FONT.render("Score "+ str(score),1 , (0,0,0))
     win.blit(text,(10,10))
-    text = STAT_FONT.render("Speed "+ str(game_speed),1 , (0,0,0))
+    text = STAT_FONT.render("Gen "+ str(gen),1 , (0,0,0))
     win.blit(text,(10,50))
+    text = STAT_FONT.render("Alive "+ str(alive_nb),1 , (0,0,0))
+    win.blit(text,(WIN_WIDTH-10-text.get_width(),10))
     ground.draw(win)
-    dino.draw(win)
+    for dino in dinos:
+        dino.draw(win)
     pygame.display.update()
 
-def main():
+def main(genomes,config):
+    global GEN
+    GEN +=1
+    nets = []
+    ge = []
+    dinos = []
+    for _,g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        dinos.append(Dino(20))
+        g.fitness = 0
+        ge.append(g)
     clock_tick_count = 0
     win = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
     win.fill([255,255,255])
@@ -201,34 +221,68 @@ def main():
     ground = Ground(WIN_HEIGHT-100)
     died = False
     run = True
-    dino = Dino(20)
     obstacles = [Obstacle(WIN_WIDTH+10,0)]
     score = 0
-    game_speed = 7
+    game_speed = 1
     
     while run:
-        clock.tick(60)
+        clock.tick(120)
         clock_tick_count += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 quit()
-            dino.input_handler(pygame.key.get_pressed())
-        if died == False:
-            score+=1
-            if (score%200==0):
-                game_speed+=1
-            if len(obstacles)>0:
-                for obstacle in obstacles:
+            #dino.input_handler(pygame.key.get_pressed())
+        obstacle_ind = 0
+        if len(dinos)>0:
+            if len(obstacles)>1 and dinos[0].x > round(obstacles[0].x) + obstacles[0].img.get_width():
+                obstacle_ind = 1
+        else:
+            run = False
+            break
+        rem = []
+        score+=1
+        if (score%20==0):
+            game_speed+=0.01
+        if len(obstacles)>0:
+            for obstacle in obstacles:
+                for x,dino in enumerate(dinos):
                     if obstacle.collide(dino):
-                        died=True
-                    obstacle.move(game_speed)
-            obstacles = obstacle_delete(obstacles)
-            if clock_tick_count==60:
-                obstacles = obstacle_gen(obstacles)
-                clock_tick_count=0
+                        ge[x].fitness -= 1
+                        dinos.pop(x)
+                        nets.pop(x)
+                        ge.pop(x)
+                obstacle.move(game_speed)
+        
+        
+        if clock_tick_count==60:
+            obstacles = obstacle_gen(obstacles)
+            clock_tick_count=0
+        for x,dino in enumerate(dinos):
+            ge[x].fitness+=0.05
             dino.move()
-            ground.move(game_speed)
-        draw_window(win,dino,ground,obstacles,score,game_speed)
-main()
+            if(len(obstacles)>0):
+                output = nets[x].activate((dino.y, abs(dino.y - obstacles[obstacle_ind].y),game_speed,obstacles[obstacle_ind].x))
+                dino.input_handler(output[0],output[1])
+        obstacles = obstacle_delete(obstacles)
+        ground.move(game_speed)
+        draw_window(win,dinos,ground,obstacles,score,game_speed,GEN,len(dinos))
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main,50)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
